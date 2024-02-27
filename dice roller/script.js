eccecf822cc16fb684475eda60a93457de354ed6
Sasha <136998728+SillyBoySasha@users.noreply.github.com>
2024-02-27 18:27:52 +0300

    321

---
+++ dice roller/script.js
@@ -0,0 +1,16 @@
+function rollDice(){
+
+    const numOfDice = document.getElementById("numOfDice").value;
+    const diceResult = document.getElementById("diceResult");
+    const diceImage = document.getElementById("diceImage");
+    const values = [];
+    const images = [];
+
+    for(let i = 0; i < numOfDice; i++){
+        const value = Math.floor(Math.random() * 6) + 1;
+        values.push(value);
+        images.push(`<img src="img/${value}.png">`);
+    }
+    diceResult.textContent = `dice: ${values.join(',')}`;
+    diceImage.innerHTML = images.join('');
+}
